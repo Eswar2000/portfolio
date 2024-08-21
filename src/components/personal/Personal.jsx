12020de955c41f5ae63b40dc71a11787d0379a4e
Eswar2000 <v.eswarraman2000@gmail.com>
2024-08-21 14:01:15 +0530

    Completed know me more page personal info.

--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -3,7 +3,15 @@ import './personal.scss';
 export default function Personal() {
   return (
     <div className='personal'>
-        Personal Page
+        <div className="personal-left">
+          <div className="blob" />
+        </div>
+        <div className="personal-right">
+          <h2 className='section-header'>About</h2>
+          <p className="about-me">As a Software Consulting Engineer at Cisco, I specialize in developing, testing, and deploying cutting-edge network automation solutions 🌐 for leading enterprise and service provider networks. My role allows me to leverage my expertise in creating robust systems that enhance network efficiency and reliability. With a track record of delivering high-impact solutions, I thrive on tackling complex challenges through innovation 💡. My passion for technology drives me to stay ahead of industry trends 🚀 and continually refine my skills.</p>
+          <p className="about-me"></p>
+          <p className="about-me">When I am not immersed at work, I enjoy gaming 🎮, travelling ✈️ and playing outdoor games like cricket 🏏, football ⚽ and basketball 🏀. These activities offer a refreshing balance to my tech-focused career, allowing me to unwind and stay active. Exploring new places, engaging in strategic games, and participating in team sports help me recharge and inspire creativity at my work.</p>
+        </div>
     </div>
   )
 }
