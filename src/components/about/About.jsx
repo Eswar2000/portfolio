e3b76b590b29b1c103faf17795f92e98ff2c59c8
Eswar2000 <v.eswarraman2000@gmail.com>
2024-08-21 14:05:04 +0530

    Boiler plate for education is ready

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,13 @@
 import './about.scss';
 import Personal from '../personal/Personal';
 import Merits from '../merits/Merits';
+import Education from '../education/Education';
 
 export default function About() {
   return (
     <div className='sections'>
         <Personal />
+        <Education />
         <Merits />
     </div>
   )
---
+++ src/components/education/Education.jsx
@@ -0,0 +1,9 @@
+import './education.scss';
+
+export default function Education() {
+  return (
+    <div className='education'>
+        Education
+    </div>
+  )
+}
