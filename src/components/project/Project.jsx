c41b1e4f071158eea80e4c586556ffde6e4475ca
Eswar2000 <v.eswarraman2000@gmail.com>
2024-08-31 15:15:34 +0530

    Added additional projects and added capability to conditionally give github urrl

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -7,12 +7,26 @@ export default function Project() {
 
   const project_list = [
     {
+      "name": "Network Telemetry Simulator",
+      "completion": "August 2023*",
+      "image": "assets/telemetry-simulator-small.png",
+      "description": "A network telemetry data simulator is a prototype being developed to address bottlenecks in project development and POCs caused by limited network devices or permissions. By using mathematical modeling and calibration, it generates realistic network traffic data, improving observability and testing capabilities for performance and security analysis.",
+      "skills": ["NodeJS", "ExpressJS", "MongoDB", "YANG", "IOS-XR", "IOS-XE", "Gaussian Distribution", "Poisson Distribution"]
+    },
+    {
       "name": "Faculty Dashboard",
       "completion": "September 2021",
       "image": "assets/faculty-dashboard-small.png",
       "git_url": "https://github.com/Eswar2000/faculty-dashboard",
       "description": "A streamlined web application developed with the MERN stack, leveraging Agile methodologies and Scrum practices. This platform centralizes essential faculty tasks into a single interface—managing lecture schedules, student gate passes and leaves, and mentoring diaries. Its architectural design and streamlined processes have been shown to enhance efficiency by up to 30%.",
       "skills": ["MongoDB", "ExpressJS", "ReactJS", "NodeJS", "Jira", "Agile", "Scrum", "SDLC", "Mocha Unit Testing", "Selenium UI Testing"]
+    },
+    {
+      "name": "Conflict Network Evaluation",
+      "completion": "April 2022",
+      "image": "assets/conflict-network-small.png",
+      "description": "Our project aims to develop a flexible framework for extracting, quantifying, and representing conflict events as networks. By analyzing social media interactions, especially on platforms like Twitter, we seek to identify patterns, quantify influences, and predict potential conflicts, providing insights into social dynamics.",
+      "skills": ["Python", "Graph Networks", "Social Network Analysis", "Exploratory Data Analysis", "Sentiment Analysis", "ACLED", "Computational Social Science"]
     },
     {
       "name": "Note Maker",
@@ -29,6 +43,13 @@ export default function Project() {
       "git_url": "https://github.com/Eswar2000/maze-game-racket",
       "description": "A coursework project developed using Racket to demonstrate various SICP concepts through an engaging maze game. Players navigate a protagonist from the source to the destination, overcoming obstacles and challenges to earn points. Successfully completing each level unlocks new, more complex mazes, with increasing difficulty.",
       "skills": ["Racket", "SICP"]
+    },
+    {
+      "name": "Spotify Song Recommendation",
+      "completion": "December 2021",
+      "image": "assets/song-recommendation-small.png",
+      "description": "A content-based filtering recommendation system that utilizes the Spotify songs dataset and cosine similarity to suggest relevant tracks. The system features enhanced accuracy through improved feature extraction and hybrid techniques, optimizing recommendation relevance and overall performance.",
+      "skills": ["Python", "Kaggle", "Content-based filtering recommendation system", "Exploratory Data Analysis", "Data Imputation", "PCA", "Correlation Analysis", "Data Encoding", "Data Normalization"]
     },
     {
       "name": "Higher Studies Portal",
@@ -85,7 +106,12 @@ export default function Project() {
                         </div>
                         <div className="project-right-container">
                             <p>{project.description}</p>
-                            <a href={project.git_url} target='_blank' rel='noreferrer'>Github Link &rarr;</a>
+                            {
+                              project.git_url && <a href={project.git_url} target='_blank' rel='noreferrer'>Github Link &#128275;</a>
+                            }
+                            {
+                              !project.git_url && <a href='#' rel='noreferrer'>Github Link &#128274;</a>
+                            }
                             <h3>{project.completion}</h3>
                             <div className="chip-list">
                               {
